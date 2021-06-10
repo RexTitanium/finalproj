3bf038f60b52c5707e27fa4c0939873c0d85cbc9
Samyak Shah <s4samyak@gmail.com>
2021-06-11 02:51:56 +0530

    netlify is good

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,7 +58,7 @@ class Contact extends Component{
                      animate={{y:0, opacity: 1}}
                      transition ={{ type:'spring', stiffness: 150, delay:0.5}}
                      >
-                        <p>Form Service is currently down and will soon be up. If you want to connect to me , you can mail me at</p>
+                        <p>If you have any questions or want to collaborate just fill out the form, and I will answer back to you shortly. If you want to contact me directly, you can mail me at</p>
                         <Tooltip TransitionComponent={Zoom} title="Gmail" arrow>
                             <a href="mailto:s4samyak@gmail.com" className="mail-link"> s4samyak@gmail.com</a>
                         </Tooltip>
@@ -70,7 +70,7 @@ class Contact extends Component{
                  
                  <div className=" col-15 col-md-5">
                     <div className="contact-form">
-                        <form className="contact-form" onSubmit={this.handleSubmit} name="contact" method="post" data-netlify="true"  data-netlify-honeypot="bot-field" >
+                        <form className="contact-form" onSubmit={this.handleSubmit} name="contact" method="post" >
                          <input type="hidden" name="form-name" value="contact" />
                             <p className="group">      
                                 <input type="text" name="name" value={name} onChange={this.handleChange} required />
