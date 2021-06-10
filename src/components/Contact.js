39c7a486e0068b6582cc62bad961257b47266a7e
Samyak Shah <s4samyak@gmail.com>
2021-06-11 02:32:16 +0530

    netlify is shit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,7 +58,7 @@ class Contact extends Component{
                      animate={{y:0, opacity: 1}}
                      transition ={{ type:'spring', stiffness: 150, delay:0.5}}
                      >
-                        <p>If you have any questions or want to collaborate just fill out the form, and I will answer back to you shortly. If you want to contact me directly, you can mail me at</p>
+                        <p>Form Service is currently down and will soon be up. If you want to connect to me , you can mail me at</p>
                         <Tooltip TransitionComponent={Zoom} title="Gmail" arrow>
                             <a href="mailto:s4samyak@gmail.com" className="mail-link"> s4samyak@gmail.com</a>
                         </Tooltip>
